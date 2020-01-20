dabf624dee38431eab9250da37314999651eb363
JesseMcRae <38913985+JesseMcRae@users.noreply.github.com>
2020-01-20 18:25:41 +0800

    Add files via upload

---
+++ drawt.m
@@ -0,0 +1,11 @@
+function [] = drawt(nodes, ChannelName)
+sM = zeros(length(nodes));
+for i = 2:length(nodes)
+    if nodes(i) ~= 0
+        sM(nodes(i),i) = 1;
+    end
+end
+bgl = biograph(sM, ChannelName);
+view(bgl);
+return;
+end
